69e73f2e6be037a31bc844cefd2fe7e7eefdfa9f
XavierCP7 <108491576+XavierCP7@users.noreply.github.com>
2022-07-05 08:41:40 -0400

    Update flanker_run.js

--- Qualtrics/flanker_run.js
+++ Qualtrics/flanker_run.js
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <title>Flanker Task</title>
+  <script src="Flanker_Qualtrics/Qualtrics/jspsych.js"></script>
+  <script src="Flanker_Qualtrics/Qualtrics/plugins/jspsych-html-keyboard-response.js"></script>
+  <script src="Flanker_Qualtrics/Qualtrics/plugins/jspsych-image-keyboard-response.js"></script>
+  <link rel="stylesheet" href="Flanker_Qualtrics/Qualtrics/css/jspsych.css">
+  <script src="Qualtrics/flanker_main.js"></script>
+</head>
+
+<body>
+</body>
+<script>
+  /*start experiment*/
+  jsPsych.init({
+    timeline: timeline,
+    on_finish: function () {
+      jsPsych.data.displayData();
+    }
+  });
+</script>
+
+</html>
