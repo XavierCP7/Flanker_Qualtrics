0e10cc6cc417d0442a3a681094bfbb286d7e413d
XavierCP7 <108491576+XavierCP7@users.noreply.github.com>
2022-07-06 10:17:46 -0400

    Update flanker_main.js

--- Qualtrics/flanker_main.js
+++ Qualtrics/flanker_main.js
@@ -11,7 +11,18 @@
      
   };
 
-  /*defining stimuli*/
+/*set up instructionsP block*/
+  var instructionsP = {
+    type: "html-keyboard-response",
+    stimulus: "<p>Dans cette tâche, tu verras apparaitre cinq flèches à l’écran. Exactement comme l’exemple que tu viens d’avoir.</p>" +
+      "<img src='img/inc1.png'></img>" +
+      "<p>Appui sur la flèche gauche de ton clavier si la flèche à l’écran pointe à gauche. (<)</p>" +
+      "<p>Appui sur la flèche droite de ton clavier si la flèche à l’écran pointe à droite. (>)</p>" +
+      "<p>Lorsque tu es prêt, appui sur n’importe quelle touche de ton clavier pour commencer un essai de pratique.</p>",
+    post_trial_gap: 1000
+  };
+
+ /*defining stimuli*/
   var test_stimuli_p = [
     {
       stimulus: "img/con1.png",
@@ -30,6 +41,45 @@
       data: { stim_type: 'incongruent', direction: 'left'}
     }
   ];
+
+/* defining testp timeline */
+  var testp = {
+    timeline: [{
+      type: 'image-keyboard-response',
+      choices: [37, 39],
+      trial_duration: 1500,
+      stimulus: jsPsych.timelineVariable('stimulus'),
+      data: jsPsych.timelineVariable('data'),
+      on_finish: function (data) {
+        var correct = false;
+        if (data.direction == 'left' && data.key_press == 37 && data.rt > -1) {
+          correct = true;
+        } else if (data.direction == 'right' && data.key_press == 39 && data.rt > -1) {
+          correct = true;
+        }
+        data.correct = correct;
+      },
+      post_trial_gap: function () {
+        return Math.floor(Math.random() * 1500) + 500;
+      }
+    }],
+    timeline_variables: test_stimuli_p,
+    sample: {
+      type: 'fixed-repetitions',
+      size: reps_per_trial_type_p
+    }
+  };
+
+  /*set up instructions1 block*/
+  var instructions1 = {
+    type: "html-keyboard-response",
+    stimulus: "<p>Dans cette tâche, tu verras apparaitre cinq flèches à l’écran. Exactement comme l’exemple que tu viens d’avoir.</p>" +
+      "<img src='img/inc1.png'></img>" +
+      "<p>Appui sur la flèche gauche de ton clavier si la flèche à l’écran pointe à gauche. (<)</p>" +
+      "<p>Appui sur la flèche droite de ton clavier si la flèche à l’écran pointe à droite. (>)</p>" +
+      "<p>Appui sur n’importe quelle touche de ton clavier lorsque tu es prêt à commencer le vrai test.</p>",
+    post_trial_gap: 1000
+  };
 /*defining stimuli*/
   var test_stimuli_t = [
       {
@@ -133,57 +183,6 @@
       data: { stim_type: 'incongruent', direction: 'left'}
     }
   ];
-/*set up instructionsP block*/
-  var instructionsP = {
-    type: "html-keyboard-response",
-    stimulus: "<p>Dans cette tâche, tu verras apparaitre cinq flèches à l’écran. Exactement comme l’exemple que tu viens d’avoir.</p>" +
-      "<img src='img/inc1.png'></img>" +
-      "<p>Appui sur la flèche gauche de ton clavier si la flèche à l’écran pointe à gauche. (<)</p>" +
-      "<p>Appui sur la flèche droite de ton clavier si la flèche à l’écran pointe à droite. (>)</p>" +
-      "<p>Lorsque tu es prêt, appui sur n’importe quelle touche de ton clavier pour commencer un essai de pratique.</p>",
-    post_trial_gap: 1000
-  };
-
-
-/* defining testp timeline */
-  var testp = {
-    timeline: [{
-      type: 'image-keyboard-response',
-      choices: [37, 39],
-      trial_duration: 1500,
-      stimulus: jsPsych.timelineVariable('stimulus'),
-      data: jsPsych.timelineVariable('data'),
-      on_finish: function (data) {
-        var correct = false;
-        if (data.direction == 'left' && data.key_press == 37 && data.rt > -1) {
-          correct = true;
-        } else if (data.direction == 'right' && data.key_press == 39 && data.rt > -1) {
-          correct = true;
-        }
-        data.correct = correct;
-      },
-      post_trial_gap: function () {
-        return Math.floor(Math.random() * 1500) + 500;
-      }
-    }],
-    timeline_variables: test_stimuli_p,
-    sample: {
-      type: 'fixed-repetitions',
-      size: reps_per_trial_type_p
-    }
-  };
-
-  /*set up instructions1 block*/
-  var instructions1 = {
-    type: "html-keyboard-response",
-    stimulus: "<p>Dans cette tâche, tu verras apparaitre cinq flèches à l’écran. Exactement comme l’exemple que tu viens d’avoir.</p>" +
-      "<img src='img/inc1.png'></img>" +
-      "<p>Appui sur la flèche gauche de ton clavier si la flèche à l’écran pointe à gauche. (<)</p>" +
-      "<p>Appui sur la flèche droite de ton clavier si la flèche à l’écran pointe à droite. (>)</p>" +
-      "<p>Appui sur n’importe quelle touche de ton clavier lorsque tu es prêt à commencer le vrai test.</p>",
-    post_trial_gap: 1000
-  };
-
   /* defining test1 timeline */
   var test1 = {
     timeline: [{
